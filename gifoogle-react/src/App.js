da8030bc9d47e9bbbbb5e5f0a924c0998c8e1923
Ahsan Yousaf <ahsan.yousaf@southvillesol.com>
2019-03-07 17:06:41 +0500

    add bootstrap

--- gifoogle-react/src/App.js
+++ gifoogle-react/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App container">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
