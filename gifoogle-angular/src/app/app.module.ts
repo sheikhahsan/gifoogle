17a8942e8abc83e3a11852360d0777419cc035ac
Ahsan Yousaf <ahsan.yousaf@southvillesol.com>
2019-03-15 17:41:56 +0500

    add bootstrap and navbar component

--- gifoogle-angular/src/app/app.module.ts
+++ gifoogle-angular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
