b730cfc1a0ea0659adc714db29515dc9f21205a4
Ahsan Yousaf <ahsan.yousaf@southvillesol.com>
2019-03-18 16:40:18 +0500

    add randomGif screen

--- gifoogle-angular/src/app/pages/random/random.component.ts
+++ gifoogle-angular/src/app/pages/random/random.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from "src/app/services/data.service";
+import * as Util from "src/app/utils";
 
 @Component({
   selector: 'app-random',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./random.component.scss']
 })
 export class RandomComponent implements OnInit {
+  randomGif: Object = {};
 
-  constructor() { }
+  constructor(private data: DataService) {}
 
   ngOnInit() {
+    this.data.getRandomGif().subscribe(res => {
+      this.randomGif = res;
+      console.log(this.randomGif);
+    });
   }
 
+  getFullUrl(gifId) {
+    return Util.getFullUrl(gifId);
+  };
 }
